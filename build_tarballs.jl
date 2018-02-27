edb2b8e3fe759c1d3fbe2ba6b810b95f4a96a509
Jacob Quinn <quinn.jacobd@gmail.com>
2018-02-27 07:06:46 -0700

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,25 +2,35 @@ using BinaryBuilder
 
 # Collection of sources required to build MySQL
 sources = [
-
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-x86_64.tar.gz" =>
-    "149102915ea1f1144edb0de399c3392a55773448f96b150ec1568f700c00c929",
-
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-i686.tar.gz" =>
-    "32e463fda6613907b90d44228b3b81ad7508ce5e20a928b86ced47fbce1fe92a",
-
+    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-i686.tar
+.gz" => "32e463fda6613907b90d44228b3b81ad7508ce5e20a928b86ced47fbce1fe92a",
+    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-x86_64
+.tar.gz" => "149102915ea1f1144edb0de399c3392a55773448f96b150ec1568f700c00c929",
+    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-win32.zip" => "a32487407bc0c4e21
+7d8839892333fb0cb39153194d2788f226e9c5b9abdd928",
+   "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-winx64.zip" => "3555641cea2da6
+0435ab7f1681a94d1aa97341f1a0f52193adc82a83734818ca",
+    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-macos10.12-x86_64.tar.gz" => "c97d76936
+c6caf063778395e7ca15862770a1ab77c1731269408a8d5c0eb4b93",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-if [ $target != "i686-linux-gnu" ]; then
+if [ $target == "i686-linux-gnu" ]; then
 cd $WORKSPACE/srcdir
-cp mysql-connector-c-6.1.11-linux-glibc2.12-x86_64/lib/libmysqlclient.so.18.4. $prefix/libmysqlclient.so
-
-else
+cp mysql-connector-c-6.1.11-linux-glibc2.12-i686/lib/libmysqlclient.so.18.4. $prefix/lib/libmysqlclient.so
+elif [ $target == "x86_64-linux-gnu" ]; then
 cd $WORKSPACE/srcdir
-cp mysql-connector-c-6.1.11-linux-glibc2.12-i686/lib/libmysqlclient.so.18.4. $prefix/libmysqlclient.so
-
+cp mysql-connector-c-6.1.11-linux-glibc2.12-x86_64/lib/libmysqlclient.so.18.4. $prefix/lib/libmysqlclient.so
+elif [ $target == "i686-w64-mingw32" ]; then
+cd $WORKSPACE/srcdir
+cp mysql-connector-c-6.1.11-win32/lib/libmysql.dll $prefix/lib/libmysqlclient.dll
+elif [ $target == "x86_64-w64-mingw32" ]; then
+cd $WORKSPACE/srcdir
+cp mysql-connector-c-6.1.11-winx64/lib/libmysql.dll $prefix/lib/libmysqlclient.dll
+elif [ $target == "x86_64-apple-darwin14" ]; then
+cd $WORKSPACE/srcdir
+cp mysql-connector-c-6.1.11-macos10.12-x86_64/lib/libmysqlclient.18.dylib $prefix/lib/libmysqlclient.dylib
 fi
 
 """
@@ -28,8 +38,11 @@ fi
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc)
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Windows(:i686),
+    Windows(:x86_64),
+    MacOS()
 ]
 
 # The products that we will ensure are always built
