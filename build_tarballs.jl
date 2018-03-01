2e705503eb3d0d3534ab74668fbc9cee56f39c6e
quinnj <quinn.jacobd@gmail.com>
2018-03-01 08:10:05 -0700

    Refactor builds per platform, focusing on mariadb sources, except osx where, the apple sdk from BinaryBuilder is too old to build it. For osx, we just use the mysql pre-packaged tarball. For windows, we leverage pre-packaged binaries from mariadb and appveyor to unpack and perform the same github release process as travis

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,29 +2,24 @@ using BinaryBuilder
 
 # Collection of sources required to build MySQL
 sources = [
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-i686.tar.gz" => "32e463fda6613907b90d44228b3b81ad7508ce5e20a928b86ced47fbce1fe92a",
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-linux-glibc2.12-x86_64.tar.gz" => "149102915ea1f1144edb0de399c3392a55773448f96b150ec1568f700c00c929",
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-win32.zip" => "a32487407bc0c4e217d8839892333fb0cb39153194d2788f226e9c5b9abdd928",
-   "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-winx64.zip" => "3555641cea2da60435ab7f1681a94d1aa97341f1a0f52193adc82a83734818ca",
-    "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-macos10.12-x86_64.tar.gz" => "c97d76936c6caf063778395e7ca15862770a1ab77c1731269408a8d5c0eb4b93",
+    "https://downloads.mariadb.com/Connectors/c/connector-c-3.0.3/mariadb-connector-c-3.0.3-src.tar.gz" =>
+    "210f0ee3414b235d3db8e98e9e5a0a98381ecf771e67ca4a688036368984eeea",
+     "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-macos10.12-x86_64.tar.gz" =>
+    "c97d76936c6caf063778395e7ca15862770a1ab77c1731269408a8d5c0eb4b93",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-mkdir -p $prefix/lib
-if [ $target == "i686-linux-gnu" ]; then
-cp mysql-connector-c-6.1.11-linux-glibc2.12-i686/lib/libmysqlclient.so.18.4. $prefix/lib/libmysqlclient.so
-elif [ $target == "x86_64-linux-gnu" ]; then
-cp mysql-connector-c-6.1.11-linux-glibc2.12-x86_64/lib/libmysqlclient.so.18.4. $prefix/lib/libmysqlclient.so
-elif [ $target == "i686-w64-mingw32" ]; then
-cp mysql-connector-c-6.1.11-win32/lib/libmysql.dll $prefix/lib/libmysqlclient.dll
-elif [ $target == "x86_64-w64-mingw32" ]; then
-cp mysql-connector-c-6.1.11-winx64/lib/libmysql.dll $prefix/lib/libmysqlclient.dll
-elif [ $target == "x86_64-apple-darwin14" ]; then
-cp mysql-connector-c-6.1.11-macos10.12-x86_64/lib/libmysqlclient.18.dylib $prefix/lib/libmysqlclient.dylib
+if [ $target == "x86_64-apple-darwin14" ]; then
+    cd $WORKSPACE/srcdir
+    mkdir $prefix/lib
+    cp mysql-connector-c-6.1.11-macos10.12-x86_64/lib/libmysqlclient.18.dylib $prefix/lib/libmariadb.dylib
+else
+    cd $WORKSPACE/srcdir
+    cd mariadb-connector-c-3.0.3-src/
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+    make && make install
 fi
-
 """
 
 # These are the platforms we will build for by default, unless further
@@ -32,14 +27,15 @@ fi
 platforms = [
     Linux(:i686, :glibc),
     Linux(:x86_64, :glibc),
-    #Windows(:i686), see https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/167
-    Windows(:x86_64),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc),
+    Linux(:powerpc64le, :glibc),
     MacOS()
 ]
 
 # The products that we will ensure are always built
 products(prefix) = Product[
-    LibraryProduct(prefix, "libmysqlclient", :libmysql)
+    LibraryProduct(prefix, "libmariadb", :libmariadb)
 ]
 
 # Dependencies that must be installed before this package can be built
---
+++ build_windows_tarballs.jl
@@ -0,0 +1,9 @@
+using BinaryBuilder
+
+const prefix = Prefix("products\\libmariadb.dll")
+
+const platform = ENV["BIT"] == "32" ? Windows(:i686) : Windows(:x86_64)
+
+file, hash = package(prefix, "products\\MySQL"; platform=platform, verbose=true, force=true)
+
+println("successfully packaged (\"$file\", \"$hash\")")
