6efb807552663235528b8f3c2b1c7dd75fd89098
quinnj <quinn.jacobd@gmail.com>
2018-03-01 09:22:27 -0700

    Fixes for appveyor

--- build_windows_tarballs.jl
+++ build_windows_tarballs.jl
@@ -1,9 +1,9 @@
 using BinaryBuilder
 
-const prefix = Prefix("products")
+const prefix = Prefix("")
 
 const platform = ENV["BIT"] == "32" ? Windows(:i686) : Windows(:x86_64)
 
-file, hash = package(prefix, "products\\MySQL"; platform=platform, verbose=true, force=true)
+file, hash = package(prefix, "MySQL"; platform=platform, verbose=true, force=true)
 
 println("successfully packaged (\"$file\", \"$hash\")")
